29ca2e8a28e0425c806046d8076f3d5f1425cf08
ZacMcleod <zacamcleod33@gmail.com>
2023-06-21 12:18:05 -0700

    Added Delete and Put

--- app.py
+++ app.py
@@ -58,7 +58,32 @@ class ProductListResource(Resource):
         db.session.add(new_product)
         db.session.commit()
         return product_schema.dump(new_product), 201
+    
+class ProductResource(Resource):
+    def get(self, product_id):
+        product_from_db = Product.query.get_or_404(product_id)
+        return product_schema.dump(product_from_db)
+    
+    def delete(self, product_id):
+        product_from_db = Product.query.get_or_404(product_id)
+        db.session.delete(product_from_db)
+        return '', 204
 
+    def put(self, product_id):
+        product_from_db = Product.query.get_or_404(product_id)
+        
+        if 'name' in request. json:
+            product_from_db.name = request.json [ 'name' ]
+        if 'description' in request. json:
+            product_from_db.description = request.json [ 'description' ]
+        if 'price' in request. json:
+            product_from_db.price = request.json [ 'price' ]
+        if 'inventory_quantity' in request. json:
+            product_from_db.inventory_quantity = request.json [ 'inventory_quantity' ]
+
+        db.session.commit()
+        return product_schema.dump(product_from_db)
 
 # Routes
-api.add_resource(ProductListResource, "/api/products/")
+api.add_resource(ProductListResource, '/api/products/')
+api.add_resource(ProductResource, '/api/products/<int:product_id>')
